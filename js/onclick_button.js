14633167047df4d56dfdf7cee56797108aa16a92
Sankalp-Dawada <sankalpdawada@gmail.com>
2025-01-27 21:15:13 +0530

    connected the start game button

--- js/onclick_button.js
+++ js/onclick_button.js
@@ -1,9 +1,13 @@
 function dologin() {
     //alert("Login clicked!");
-    window.location.href = "../index.html";
+    window.location.href = "../pages/index.html";
 }
 
 function dosignup() {
     //alert("Signup clicked!"); 
-    window.location.href = "signup.html";
+    window.location.href = "../pages/signup.html";
+}
+
+function startgame(){
+    window.location.href = "../pages/gamepage.html";
 }
